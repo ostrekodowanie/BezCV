93f9f5dca6116f1cf709ee846da082cf56b9d7c1
Sebastian <se6359@gmail.com>
2022-12-09 20:08:41 +0100

    backend

---
+++ backend/apps/Auth/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField(max_length=255, unique=True)
+    phone = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    is_verified = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return '{} - {}'.format(
+            self.pk,
+            self.email,
+        )
