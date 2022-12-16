48dba3d5a341e6a0f0819183f9f3016ccdea11b5
Filip <filfer05@gmail.com>
2022-12-16 23:19:31 +0100

    purchases fixing

--- frontend/src/pages/Candidate.tsx
+++ frontend/src/pages/Candidate.tsx
@@ -27,6 +27,7 @@ export default function Candidate({ id, first_name, last_name, slug }: Candidate
     const { access } = auth.tokens
     const [loading, setLoading] = useState(true)
     const [candidateDetails, setCandidateDetails] = useState({
+        is_purchased: false,
         email: '',
         phone: ''
     })
@@ -34,7 +35,7 @@ export default function Candidate({ id, first_name, last_name, slug }: Candidate
     const handlePurchase = async () => {
         if(points < 1) return navigate('/punkty')
         let data = { candidate: id, employer: user_id }
-        const resp = await axios.post('/oferty/purchase', JSON.stringify(data), { headers: { 'Authorization': 'Bearer ' + access }})
+        const resp = await axios.post('/api/oferty/purchase', data)
         if(resp.status === 201) return dispatch(purchase())
     }
 
@@ -54,7 +55,7 @@ export default function Candidate({ id, first_name, last_name, slug }: Candidate
                 <h2>{candidateDetails.email ? candidateDetails.email : first_name.charAt(0).toLowerCase() + '******@*****.***'}</h2>
                 <h2>{candidateDetails.phone ? candidateDetails.phone : '+48 *** *** ***'}</h2>
             </div>
-            <button onClick={handlePurchase} className="bg-primary transition-colors max-w-max font-medium hover:bg-darkPrimary text-white rounded flex items-center py-2 px-6">Wykup kontakt za 1 punkt</button>
+            {!candidateDetails.is_purchased && <button onClick={handlePurchase} className="bg-primary transition-colors max-w-max font-medium hover:bg-darkPrimary text-white rounded flex items-center py-2 px-6">Wykup kontakt za 1 punkt</button>}
         </div>            
     )
 }
