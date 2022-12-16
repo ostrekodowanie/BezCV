7cfe954c054a55774a631cdd59a423216fd67732
Sebastian <se6359@gmail.com>
2022-12-16 14:01:09 +0100

    admin, points

--- backend/apps/Admin/urls.py
+++ backend/apps/Admin/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('admin/candidates', views.VerifyCandidatesView.as_view()),
+  path('admin/candidates', views.UnverifiedCandidatesView.as_view()),
+  path('admin/candidates/verify', views.VerifyCandidatesView.as_view()),
 ]
