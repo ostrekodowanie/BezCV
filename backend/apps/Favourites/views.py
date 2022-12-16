2e8d027cf02e4962f508426feee1b783fdefce44
Sebastian <se6359@gmail.com>
2022-12-16 16:14:55 +0100

    favourite field

--- backend/apps/Favourites/views.py
+++ backend/apps/Favourites/views.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 
 from rest_framework import generics, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
 from .models import FavouriteCandidates
@@ -9,15 +10,20 @@ from apps.Candidates.models import Candidates
 
 class FavouriteOffersView(generics.ListAPIView):
     serializer_class = serializers.FavouriteOffersSerializer
+    permission_classes = [IsAuthenticated]
+
     def get_queryset(self):
         u = self.request.GET.get('u')
         return Candidates.objects.filter(favouritecandidates_candidate__employer_id=u)
 
+
 class AddToFavouritesView(generics.CreateAPIView):
     serializer_class = serializers.FavouriteOffersSerializer
 
+
 class RemoveFromFavouritesView(generics.DestroyAPIView):
     serializer_class = serializers.FavouriteOffersSerializer
+
     def destroy(self, request, *args, **kwargs):
         u = self.kwargs['u']
         c = self.kwargs['c']
