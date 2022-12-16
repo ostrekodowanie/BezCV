1efa650b8aede1b3ef23ab2ea83467ba13ec78ab
Sebastian <se6359@gmail.com>
2022-12-16 15:42:26 +0100

    token

--- backend/apps/Candidates/models.py
+++ backend/apps/Candidates/models.py
@@ -77,4 +77,23 @@ class PurchasedOffers(models.Model):
             self.pk,
         )
 
+class FavouriteCandidates(models.Model):
+    employer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='favouritecandidates_employer')
+    candidate = models.ForeignKey(
+        Candidates, on_delete=models.CASCADE, related_name='favouritecandidates_candidate')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Favourite Candidates'
+        unique_together = [['employer', 'candidate']]
+
+    def __str__(self):
+        return '{}'.format(
+            self.pk,
+        )
+        
+
+
     
