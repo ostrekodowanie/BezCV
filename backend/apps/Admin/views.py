44b54de74af611e51114cfdff0a73e2aef6768b7
Sebastian <se6359@gmail.com>
2022-12-16 14:01:09 +0100

    admin, points

--- backend/apps/Admin/views.py
+++ backend/apps/Admin/views.py
@@ -1,10 +1,28 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from apps.Candidates.models import Candidates
+from apps.Candidates.models import Candidates, CandidateAbilities, Abilities
 from . import serializers
 
-class VerifyCandidatesView(generics.ListAPIView):
+class UnverifiedCandidatesView(generics.ListAPIView):
     queryset = Candidates.objects.filter(is_verified=False)
     serializer_class = serializers.VerifyCandidatesSerializer
     permission_classes = [IsAdminUser]
+
+class VerifyCandidatesView(APIView):
+    def post(self, request):
+        action = request.data.pop('action')
+        id = request.data.pop('id')
+        abilities = request.data.pop('abilities')
+        if action == 'verify':
+            Candidates.objects.filter(id=id).update(**request.data)
+            Candidates.objects.get(id=id).save()
+            for x in abilities:
+                CandidateAbilities.objects.create(candidate_id=id, ability=Abilities.objects.get(name=x))
+
+            return Response({'Successfully verified'})
+
+        Candidates.objects.filter(id=request.data['id']).delete()
+        return Response({'Successfully deleted'})
