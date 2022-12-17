f9e66e3e3eebadbe3ada7fe554943449120b34ed
Sebastian <se6359@gmail.com>
2022-12-17 02:17:54 +0100

    oferty all

--- backend/apps/Candidates/urls.py
+++ backend/apps/Candidates/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
   path('candidate/add', views.CandidateAddView.as_view()),
   path('candidate/abilities', views.AbilitiesView.as_view()),
   path('oferty', views.OffersView.as_view()),
+  path('oferty/all', views.AllCandidatesView.as_view()),
   path('oferty/search', views.SearchCandidateView.as_view()),
   path('oferty/purchase', views.PurchaseOfferView.as_view()),
   path('oferty/<slug><pk>', views.CandidateView.as_view()),
