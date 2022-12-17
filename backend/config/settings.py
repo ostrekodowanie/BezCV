8684b041bb43f718cd338e32f717c637afff3aca
Sebastian <se6359@gmail.com>
2022-12-17 19:02:34 +0100

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -160,6 +160,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'COERCE_DECIMAL_TO_STRING': False,
     #'DEFAULT_RENDERER_CLASSES': (
     #    'rest_framework.renderers.JSONRenderer',
     #),
