df33d693e2ea09ffc95ed30659a9935558d40fb6
Filip <filfer05@gmail.com>
2022-12-17 17:45:50 +0100

    slug

--- frontend/src/utils/slugFrom.tsx
+++
@@ -1,5 +0,0 @@
-const slugFrom = (name: string, surname: string, id: number) => {
-    return [name.toLowerCase().split(' ').join('-'), surname.toLowerCase().split(' ').join('-'), id].join('-')
-}
-
-export default slugFrom
