c268570d47cd7131af63fae4e0b5526507da3496
Sebastian <se6359@gmail.com>
2023-03-21 17:52:33 +0100

    Update views.py

--- backend/apps/Survey/views.py
+++ backend/apps/Survey/views.py
@@ -126,34 +126,33 @@ class CheckCodeView(APIView):
     def post(self, request):
         phone = request.data.get('phone')
         code = request.data.get('code')
-        
         try:
-            gen_code = GeneratedCodes.objects.get(phone=phone, code=code)
-        except GeneratedCodes.DoesNotExist:
-            return Response({'Access code is not valid'}, status=400)
-        
-        if Candidates.objects.filter(phone=phone).exists():
+            try:
+                gen_code = GeneratedCodes.objects.get(phone=phone, code=code)
+            except GeneratedCodes.DoesNotExist:
+                return Response({'Access code is not valid'}, status=400)
+            
             candidate = Candidates.objects.get(phone=phone)
-        else:
-            return Response({'Phone number is available.'}, status=204)
 
-        if (datetime.datetime.now(datetime.timezone.utc) - gen_code.created_at).total_seconds() <= 600:
-            completed_categories = set()
-            answered_questions = candidate.candidateanswers_candidate.objects.all().select_related('question')
+            if (datetime.datetime.now(datetime.timezone.utc) - gen_code.created_at).total_seconds() <= 600:
+                completed_categories = set()
+                answered_questions = candidate.candidateanswers_candidate.objects.all().select_related('question')
 
-            for answer in answered_questions:
-                categories = set(answer.question.category.all())
-                if categories:
-                    completed_categories.update(categories)
+                for answer in answered_questions:
+                    categories = set(answer.question.category.all())
+                    if categories:
+                        completed_categories.update(categories)
 
-            category_dict = {}
-            for category in Categories.objects.all():
-                category_questions = category.questions_category.objects.all()
-                user_questions = answered_questions.filter(question__in=category_questions)
-                if len(user_questions) == len(category_questions):
-                    category_dict[category.name] = True
-                else:
-                    category_dict[category.name] = False
-            return Response(category_dict, status=200)
-        else:
-            return Response({'Access code expired'}, status=400)
+                category_dict = {}
+                for category in Categories.objects.all():
+                    category_questions = category.questions_category.objects.all()
+                    user_questions = answered_questions.filter(question__in=category_questions)
+                    if len(user_questions) == len(category_questions):
+                        category_dict[category.name] = True
+                    else:
+                        category_dict[category.name] = False
+                return Response(category_dict, status=200)
+            else:
+                return Response({'Access code expired'}, status=400)
+        except:
+            return Response(status=204)
