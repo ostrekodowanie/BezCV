b60c45723908c0f788fde3ff3650f261a6cf62fc
Sebastian <se6359@gmail.com>
2023-04-14 01:46:18 +0200

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -30,6 +30,7 @@ SECRET_KEY = os.environ['BEZCV_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=True)
+SECURE_SSL_REDIRECT = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'bezcv.com', 'bezcv.herokuapp.com']
 
