a323829eb88a896d1e6c3c2f048e89b2f6dde6e1
Filip <filfer05@gmail.com>
2023-05-20 02:36:41 +0200

    payu

--- frontend/src/pages/points/Summary.tsx
+++ frontend/src/pages/points/Summary.tsx
@@ -47,7 +47,7 @@ export default function Summary() {
           price: foundPackage.price,
         })
       )
-      .then((res) => navigate(res.data))
+      .then((res) => (window.location.href = res.data))
       .finally(() => setLoading(false));
   };
 
