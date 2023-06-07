9a8bb7d6711725d3273c3f46d8274bafcc6f800b
Sebastian <se6359@gmail.com>
2023-06-07 15:11:30 +0200

    .

--- backend/apps/Codes/views.py
+++ backend/apps/Codes/views.py
@@ -1,4 +1,4 @@
-from rest_framework import views, status, generics
+from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
