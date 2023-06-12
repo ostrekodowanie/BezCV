f3bded9de781ef2a6eca306b80c8b65b0020042f
Sebastian <se6359@gmail.com>
2023-06-12 17:00:01 +0200

    Update serializers.py

--- backend/apps/Auth/serializers.py
+++ backend/apps/Auth/serializers.py
@@ -82,9 +82,9 @@ class UserSerializer(serializers.ModelSerializer):
         else:
             purchased_contacts = 0
             
-        tokens_from_codes = obj.usedcodes_user.aggregate(total_value=Sum('code__value')) or 0
+        tokens_from_codes = obj.usedcodes_user.aggregate(total_value=Sum('code__value'))['total_value'] or 0
 
-        remaining_tokens = purchased_tokens - purchased_contacts + tokens_from_codes['total_value']
+        remaining_tokens = purchased_tokens - purchased_contacts + tokens_from_codes
         
         return remaining_tokens + 10000
 
